1ae51e0247a020baf49284a5bbb45f338427595c
Abraham Ho <whoabe@gmail.com>
2019-06-29 17:05:52 +0800

    modified view function for detecting text

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 import config
 from flask import Flask, render_template, request
 from models.base_model import db
+from google.cloud import vision
+import io
+import cv2
+import numpy as np
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'splits_web')
@@ -75,12 +79,42 @@ def upload_file():
         user.profile_image = str(current_user.id) + "-" + file.filename
         if user.save():
             flash("profile image updated")
-            return render_template('users/user_edit.html', user = current_user)
+            return render_template('users/user_edit.html')
         # return str(output)
 
 
     else:
         flash('wrong content type')
-        return render_template('users/user_edit.html', user = current_user)
+        return render_template('users/user_edit.html')
 
-'''need another view function that sends the image to google vision AI has method=POST'''
+
+
+'''send image url to google vision AI and then put the text location and text in a database'''
+@app.route("/", methods=["POST"])
+def detect_text_uri(uri):
+    #setting coords as a dictionary
+
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+    #need to define the url in above line
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    for text in texts:
+        text_value = text.description
+        #text_value is going to the database
+        # print('\n"{}"'.format(text.description))
+        
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+        # only need the topleft and bottom right vertices
+        topleft = tuple((text.bounding_poly.vertices[0].x, text.bounding_poly.vertices[0].y))
+        
+        bottomright = tuple((text.bounding_poly.vertices[2].x, text.bounding_poly.vertices[2].y))
+        
+        box = tuple((topleft,bottomright))
+        # box is going to the database
+        
+        # print('bounds: {}'.format(','.join(vertices)))
